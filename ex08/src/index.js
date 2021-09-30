e04ed3fd6f5784f1cec9e135e4896dfa131d237f
j45m1n <jasmin.dzanovic@gmail.com>
2021-09-30 10:11:30 +0200

    Day17

---
+++ ex08/src/index.js
@@ -0,0 +1,18 @@
+const _ = require('lodash');
+
+
+
+
+function findUser(lastName, gender) {
+  try {
+
+  } catch (error) {
+      return
+      console.log();
+  }
+}
+
+getUsers();
+findUser();
+
+module.exports = findUser;
